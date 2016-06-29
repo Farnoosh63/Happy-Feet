03358794ce703cc24c4eb8016956dcc26da6717f
Farnoosh Johnson and Inthrayuth Mahaphol <inthrayuth@>
2016-06-29 14:22:29 -0700

    add ImageClick for both man and woman icon as well as showing the listview of categories for each imagebutton using adaptor

--- app/src/main/java/com/epicodus/happyfeet/WelcomeActivity.java
+++ app/src/main/java/com/epicodus/happyfeet/WelcomeActivity.java
@@ -1,13 +1,48 @@
 package com.epicodus.happyfeet;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
+import android.widget.ListView;
 
-public class WelcomeActivity extends AppCompatActivity {
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class WelcomeActivity extends AppCompatActivity implements View.OnClickListener {
+    public static final String TAG = MainActivity.class.getSimpleName();
+    @Bind(R.id.womanImageButton) ImageButton mWomanImageButton;
+    @Bind(R.id.womanListView) ListView mWomanListView;
+
+    @Bind(R.id.manImageButton) ImageButton mManImageButton;
+    @Bind(R.id.manListView) ListView mManListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+        ButterKnife.bind(this);
+
+        mWomanImageButton.setOnClickListener(this);
+        mManImageButton.setOnClickListener(this);
     }
+
+    @Override
+    public void onClick(View v) {
+        if(v == mWomanImageButton) {
+            String[] category = {"shoes", "purses", "make up", "clearance"};
+            ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, category);
+            mWomanListView.setAdapter(adapter);
+        }else if(v == mManImageButton) {
+            String[] category = {"shoes", "bags", "suits", "clearance"};
+            ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, category);
+            mManListView.setAdapter(adapter);
+        }
+
+    }
+
 }
